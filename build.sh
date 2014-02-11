01976cc0575db4ab844bbd661486276cb804e698
peko <peko@gasubasu.com>
2014-02-12 01:42:37 +0400

    update

--- build.sh
+++ build.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 f=TimeTracker
 cat $f-header.xml > $f.xml
-coffee -cb module.coffee & cat module.js >> $f.xml
+# coffee -cb module.coffee & cat module.js >> $f.xml
+cat module.js >> $f.xml
 echo '</script>]]></Content></Module>' >> $f.xml 
+git commit -am "update" & git push
